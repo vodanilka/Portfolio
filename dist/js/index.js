25811e48768d6d4f21c2368f50becdd867b1695a
Danil <142069206+vodanilka@users.noreply.github.com>
2023-10-21 15:17:04 -0700

    last section

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,6 +2,7 @@ const hamburgerMenu = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closedMenu = document.querySelector('.menu__closed');
 const overlay = document.querySelector('.menu__overlay');
+const links = document.querySelectorAll('.menu__link');
 
 hamburgerMenu.addEventListener('click', () => {
     menu.classList.add('menu__open');
@@ -11,6 +12,24 @@ closedMenu.addEventListener('click', () => {
     menu.classList.remove('menu__open');
     overlay.classList.remove('menu__overlay__active');
 });
+// closedLink.forEach( (link, i) => {
+//     link.addEventListener('click', ()=>{
+//         menu.classList.remove('menu__open');
+//         overlay.classList.remove('menu__overlay__active');
+//     });
+// });
+
+links.forEach(function(link, index) {
+    link.addEventListener('click', function() {
+
+      menu.classList.remove('menu__open');
+      overlay.classList.remove('menu__overlay__active');
+      
+    
+      
+    });
+  });
+
 
 const counters = document.querySelectorAll('.skills__progress-procents'),
       lines = document.querySelectorAll('.skills__progress-scale__inside');
